27bae6df8cdac2e5a8041a531890cdbed99b4869
Rhett Marchant <rhett.marchant@gmail.com>
2025-09-25 05:02:35 +0000

    feat: first release 0.1.0

---
+++ src/biomfg_crawler/__init__.py
@@ -0,0 +1,5 @@
+from .query import generate_chemical_query, generate_search_terms, generate_google_query
+from .crawl import google_search
+
+__all__ = ["generate_chemical_query", "generate_search_terms",
+           "generate_google_query", "google_search"]
