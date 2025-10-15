e8497358c7d53dc6ce016d3fc3cb613464478eff
Rhett Marchant <rhett.marchant@gmail.com>
2025-10-15 02:27:41 +0000

    correctly export functions

--- src/biomfg_crawler/__init__.py
+++ src/biomfg_crawler/__init__.py
@@ -1,6 +1,6 @@
-from biomfg_crawler.query import generate_chemical_query, generate_search_terms, generate_google_query
-from biomfg_crawler.crawl import google_search, extract_links
-from biomfg_crawler.download import download, submit_to_ai
+from .query import generate_chemical_query, generate_search_terms, generate_google_query
+from .crawl import google_search, extract_links
+from .download import download, submit_to_ai
 
 __all__ = ["generate_chemical_query", "generate_search_terms", "generate_google_query",
            "google_search", "extract_links",
